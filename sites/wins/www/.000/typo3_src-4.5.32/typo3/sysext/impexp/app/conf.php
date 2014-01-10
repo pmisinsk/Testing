1422216620b50f6ff7019b2deba0fe9e12276b6a
wins-dtts <wins-dtts@example.com>
2014-01-10 10:44:50 +0100

    deleted all tracked filed

--- sites/wins/www/.000/typo3_src-4.5.32/typo3/sysext/impexp/app/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('TYPO3_MOD_PATH', 'sysext/impexp/app/');
-$BACK_PATH='../../../';
-
-$MCONF['name']='xMOD_tximpexp';	// xMOD_[modulename][optional: '_something']
-
-?>
--- sites/wins/www/.000/typo3conf/ext/jquerycolorbox/ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'jQuery Colorbox');
-?>
